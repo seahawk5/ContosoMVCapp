62c9a5204e7962b57cfab64fefcb27a535ebae7f
seahawk5 <SAlishala@isdh.IN.gov>
2017-09-19 16:18:13 -0400

    Add existing files to repository

--- ContosoMVC/DAL/SchoolInitializer.cs
+++ ContosoMVC/DAL/SchoolInitializer.cs
@@ -27,13 +27,13 @@ namespace ContosoMVC.DAL
             context.SaveChanges();
             var courses = new List<Course>
             {
-            new Course{CourseID=1050,Title="Chemistry",Credits=3,},
-            new Course{CourseID=4022,Title="Microeconomics",Credits=3,},
-            new Course{CourseID=4041,Title="Macroeconomics",Credits=3,},
-            new Course{CourseID=1045,Title="Calculus",Credits=4,},
-            new Course{CourseID=3141,Title="Trigonometry",Credits=4,},
-            new Course{CourseID=2021,Title="Composition",Credits=3,},
-            new Course{CourseID=2042,Title="Literature",Credits=4,}
+            new Course{CourseID=1050,Title="Chemistry",Credits=3},
+            new Course{CourseID=4022,Title="Microeconomics",Credits=3},
+            new Course{CourseID=4041,Title="Macroeconomics",Credits=3},
+            new Course{CourseID=1045,Title="Calculus",Credits=4},
+            new Course{CourseID=3141,Title="Trigonometry",Credits=4},
+            new Course{CourseID=2021,Title="Composition",Credits=3},
+            new Course{CourseID=2042,Title="Literature",Credits=4}
             };
             courses.ForEach(s => context.Courses.Add(s));
             context.SaveChanges();
@@ -50,7 +50,7 @@ namespace ContosoMVC.DAL
             new Enrollment{StudentID=4,CourseID=4022,Grade=Grade.F},
             new Enrollment{StudentID=5,CourseID=4041,Grade=Grade.C},
             new Enrollment{StudentID=6,CourseID=1045},
-            new Enrollment{StudentID=7,CourseID=3141,Grade=Grade.A},
+            new Enrollment{StudentID=7,CourseID=3141,Grade=Grade.A}
             };
             enrollments.ForEach(s => context.Enrollments.Add(s));
             context.SaveChanges();
