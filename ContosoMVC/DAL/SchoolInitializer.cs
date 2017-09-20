7745a8b3399441588c3df252b208723abbc0b073
seahawk5 <sravanouct@gmail.com>
2017-09-19 23:05:18 -0400

    more added to student controller

--- ContosoMVC/DAL/SchoolInitializer.cs
+++ ContosoMVC/DAL/SchoolInitializer.cs
@@ -45,12 +45,13 @@ namespace ContosoMVC.DAL
             new Enrollment{StudentID=2,CourseID=1045,Grade=Grade.B},
             new Enrollment{StudentID=2,CourseID=3141,Grade=Grade.F},
             new Enrollment{StudentID=2,CourseID=2021,Grade=Grade.F},
-            new Enrollment{StudentID=3,CourseID=1050},
+            new Enrollment{StudentID=3,CourseID=1050,},
             new Enrollment{StudentID=4,CourseID=1050,},
             new Enrollment{StudentID=4,CourseID=4022,Grade=Grade.F},
             new Enrollment{StudentID=5,CourseID=4041,Grade=Grade.C},
-            new Enrollment{StudentID=6,CourseID=1045},
-            new Enrollment{StudentID=7,CourseID=3141,Grade=Grade.A}
+            new Enrollment{StudentID=6,CourseID=1045,},
+            new Enrollment{StudentID=7,CourseID=3141,Grade=Grade.A},
+            new Enrollment{StudentID=7,CourseID=1045,Grade=Grade.B}
             };
             enrollments.ForEach(s => context.Enrollments.Add(s));
             context.SaveChanges();
